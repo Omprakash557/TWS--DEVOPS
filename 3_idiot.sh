013e3fc143e235b0a5097bab79f0ba458dc96ab3
omprakash557 <omprakashjha557@gmail.com>
2026-02-28 18:35:16 +0000

    added 3_idiot.sh

---
+++ 3_idiot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#User defined variables:
+
+hero="rancho"
+villain="virus"
+
+echo "3 idiots hero is $hero"
+
+echo "3 idiots villain is $villain"
+
+# shell /environmet variables (pre-defined varibales)
+
+echo "current logged in use $USER"
+
+read -p "What is your full name?" fullname
+
+echo "my fullname is $fullname"
